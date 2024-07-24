dd213d4ecee9e0d7526748fb706204ef4142c796
dongzhihan <dongzhihan@h-partners.com>
2024-07-24 10:01:14 +0800

    fix:修改react-native-qr-decode-image-camera库harmony文件，更新版本号

--- harmony/qr_decode_image_camera/src/main/ets/RNQrDecodeImageCameraTurboModule.ts
+++ harmony/qr_decode_image_camera/src/main/ets/RNQrDecodeImageCameraTurboModule.ts
@@ -1,28 +1,19 @@
 import { TurboModule } from '@rnoh/react-native-openharmony/ts';
 import { TM } from '@rnoh/react-native-openharmony/generated/ts';
 import window from '@ohos.window';
-import picker from '@ohos.file.picker';
-import { scanCore, scanBarcode, detectBarcode ,generateBarcode,customScan } from '@kit.ScanKit';
-import { hilog } from '@kit.PerformanceAnalysisKit';
+import { scanCore, scanBarcode, detectBarcode  } from '@kit.ScanKit';
 import { BusinessError } from '@kit.BasicServicesKit';
-
-interface IPath {
-  uri:string
-}
+import Logger from "./Logger"
 
 export class RNQrDecodeImageCameraTurboModule extends TurboModule implements TM.QrDecodeImageCameraNativeModule.Spec {
-  windowClass:window.Window| undefined = undefined;
-  isKeepScreenOn: boolean = true;
-  unisKeepScreenOn: boolean = false;
 
   constructor(ctx) {
     super(ctx);
   }
 
-
 QRreader(path: TM.QrDecodeImageCameraNativeModule.IPath): Promise<unknown> {
 
-    return new Promise ((resovle,reject)=>{
+    return new Promise ((resolve,reject)=>{
       // 定义识码参数options
       let options: scanBarcode.ScanOptions = {
         scanTypes: [scanCore.ScanType.ALL],
@@ -31,18 +22,14 @@ QRreader(path: TM.QrDecodeImageCameraNativeModule.IPath): Promise<unknown> {
       }
       // 调用图片识码接口
       detectBarcode.decode(path, options).then((result: Array<scanBarcode.ScanResult>) => {
-        console.log( '[Scan Sample]', `Succeeded in getting ScanResult by promise with options, result is ${JSON.stringify(result)}`,'click success');
-        resovle(result)
+        Logger.info( '[Scan Sample]', `Succeeded in getting ScanResult by promise with options, result is ${JSON.stringify(result)}`,'click success');
+        resolve(result)
       }).catch((error: BusinessError) => {
-        console.log( '[Scan Sample]', `Failed to get ScanResult by promise with options. Code: ${error.code}, message: ${error.message}`,"click fail");
+        Logger.info( '[Scan Sample]', `Failed to get ScanResult by promise with options. Code: ${error.code}, message: ${error.message}`,"click fail");
         reject(error)
       });
     })
-
 }
-
-
-
 
   }
 

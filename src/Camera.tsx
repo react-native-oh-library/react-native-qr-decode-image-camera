e5bb39f81fbf6a8ccdef66d9ea9e64511c436206
dongzhihan <dongzhihan@h-partners.com>
2024-08-15 10:17:53 +0800

    fix:修复依赖库vision-camera相关部分代码,解决连续扫码不成功问题;

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -1,47 +1,52 @@
-import React ,{ useEffect } from 'react';
-import { Camera,useCameraDevice,  useCameraPermission } from 'react-native-vision-camera'
+import React, { useState, useEffect } from 'react';
+import {
+  PixelRatio
+} from "react-native";
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera'
 export const ComponentCamera = (props) => {
-const CODE_TYPES = [
-  'code-128',
-  'code-39',
-  'code-93',
-  'codabar',
-  'ean-13',
-  'ean-8',
-  'itf',
-  'upc-e',
-  'upc-a',
-  'qr',
-  'pdf-417',
-  'aztec',
-  'data-matrix'
-]
-const device = useCameraDevice('back');
-const {hasPermission, requestPermission} = useCameraPermission();
-if (!hasPermission) {
-  requestPermission();
-}
-const codeScanner = {
-    codeTypes: CODE_TYPES,
-    onCodeScanned: (codes)=>{
-      // console.log(`Scanned ${codes.length} codes:`, codes)
-      const value = codes[0]
-      if(value){
-        props.onRead(value)
-      }else{
-        if (value == null) return
-      }
-    },
+  const CODE_TYPES = [
+    'code-128',
+    'code-39',
+    'code-93',
+    'codabar',
+    'ean-13',
+    'ean-8',
+    'itf',
+    'upc-e',
+    'upc-a',
+    'qr',
+    'pdf-417',
+    'aztec',
+    'data-matrix'
+  ]
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  if (!hasPermission) {
+    requestPermission();
   }
+
+  const codeScanner =   {  
+      codeTypes: CODE_TYPES,
+      onCodeScanned: (codes) => {
+        // console.log(`Scanned ${codes.length} codes:`, codes)
+        const value = codes[0]
+        if (value) {
+          props.onRead(value)
+        } else {
+          if (value == null) return
+        }
+      }
+  }  
+
 
   return (
     <Camera
-          isActive={true}
-          preview={true}
-          device={device}
-          codeScanner={codeScanner}
-          zoom={props.zoom}
-          torch={props.torch ? "on" : "off"}
-        />
+      isActive={props.isActive}
+      preview={true}
+      device={device}
+      codeScanner={codeScanner}
+      torch={props.torch ? "on" : "off"}
+    /> 
   )
 }
